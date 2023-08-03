ec448498538d36019c366df8b0e0f6daf7394f8b
Sergio Venicius Vanin <sergio.vanin@capgemini.com>
2023-08-03 09:16:09 -0300

    - Script to remove s3 objects. .net calls cmd and execute a command line. Needs to have aws configured in the machine. Used to automate personal task.

---
+++ ScriptRemove/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+
+string ids = "1404,1448,1481,1564";
+const string bucketName = "h2b-web-novo";
+
+
+try
+{
+    ProcessStartInfo startInfo = new ProcessStartInfo();
+    startInfo.FileName = "cmd.exe";
+    startInfo.RedirectStandardInput = true;
+
+    using (Process process = Process.Start(startInfo))
+    {
+        if (process is null)
+        {
+            Console.WriteLine("Process created is null. Exiting.");
+            return;
+        }
+
+        foreach (string tenant in ids.Split(','))
+            process.StandardInput.WriteLine($"aws s3 rm s3://{bucketName}/{tenant} --recursive");
+
+        process.StandardInput.WriteLine("exit");
+
+        process.WaitForExit();
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine("Error: " + e.Message + e.StackTrace);
+}
+
+Console.WriteLine("Finished");
+
+Console.ReadKey();
